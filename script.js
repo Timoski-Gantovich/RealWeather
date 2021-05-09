667f2ed1434b1ea524678eb9f67d1eedf38cc964
Timoski-Gantovich <61708097+Timoski-Gantovich@users.noreply.github.com>
2021-05-08 22:12:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function weatherBalloon( cityID ) {
+  var key = 'cd71b381a8cede24534272a932e79c73';
+  fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+  .then(function(resp) { return resp.json() }) // Convert data to json
+  .then(function(data) {
+   drawWeather(data); 
+  })
+  .catch(function() {
+    // catch any errors
+  });
+}
+
+window.onload = function() {
+  weatherBalloon( 5604353 );
+}
+function drawWeather( d ) {
+var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	
+	document.getElementById('description').innerHTML = d.weather[0].description;
+	document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+}
